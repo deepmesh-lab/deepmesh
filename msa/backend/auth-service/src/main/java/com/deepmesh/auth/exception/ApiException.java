2ed1f19333f5394ecf3a53b2b764c0e02f75d463
mini-apple <suc2150@gmail.com>
2026-06-28 16:44:17 +0900

    refactor: 전역 예외처리 체계 도입

---
+++ msa/backend/auth-service/src/main/java/com/deepmesh/auth/exception/ApiException.java
@@ -0,0 +1,16 @@
+package com.deepmesh.auth.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ApiException extends RuntimeException {
+    private final HttpStatus httpStatus;
+    private final String errorCode;
+
+    public ApiException(HttpStatus httpStatus, String errorCode, String message) {
+        super(message);
+        this.httpStatus = httpStatus;
+        this.errorCode = errorCode;
+    }
+}
